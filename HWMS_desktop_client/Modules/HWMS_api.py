6e28998af08dfb274be970383ca0389d12e6920d
Max <162334963+Maxdgf@users.noreply.github.com>
2025-06-23 22:59:30 +0300

    Update HWMS_api.py
    Updating HWMS api in desctop client

--- HWMS_desktop_client/Modules/HWMS_api.py
+++ HWMS_desktop_client/Modules/HWMS_api.py
@@ -14,30 +14,36 @@ URL = 'http://ip' #esp 01 ip (replace with your esp 01 address)
 
 #gets current ip data from web page
 def get_current_ip():
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(URL)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    device_ip = soup.find('h4')
+        device_ip = soup.find('h4')
 
-    if device_ip is not None:
-        return device_ip.text
-    else:
-        return "Server is unavailable! Please, check connection."
+        if device_ip is not None:
+            return device_ip.text
+        else:
+            return "Null data!"
+    except:
+        return "current ip: ?"    
 
 #gets weather data (temperature, humidity, pressure) from web page
 def get_weather_data():
-    response = requests.get(URL)
-    response.encoding = 'UTF-8'
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:    
+        response = requests.get(URL)
+        response.encoding = 'UTF-8'
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    data_labels = soup.find_all('h1')
+        data_labels = soup.find_all('h1')
 
-    if data_labels is not None:
-        full_data = ""
+        if data_labels is not None:
+            full_data = ""
 
-        for data in data_labels:
-            full_data += re.sub(r'\s+', ' ', data.text).strip() + ' ' #convert data to 1 row string
-                
-        return full_data
-    else:
-        return "No data :(\nServer is unavailable! Please, check connection."  
+            for data in data_labels:
+                full_data += re.sub(r'\s+', ' ', data.text).strip() + ' ' #convert data to 1 row string
+                    
+            return full_data
+        else:
+            return "Null data!"  
+    except:
+        return "No data! Please check wifi connection or HWMS device."    
